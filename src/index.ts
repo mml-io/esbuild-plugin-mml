0d5434c320e9931be88d40ec89359c7ce225ac72
Daniel J. Rollins <me@deej.io>
2024-09-01 21:25:29 +0100

    export output processor types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from "./plugin";
+export type { OutputProcessor, OutputProcessorProvider } from "./results";
+export type { MMLWorldConfig } from "./world-config";
 export * from "./world-config";
