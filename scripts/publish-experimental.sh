905273d94f883bdb89e36ea84006946a723f2aca
Daniel J. Rollins <me@deej.io>
2024-09-02 13:34:11 +0100

    remove dry-run and make releases public

--- scripts/publish-experimental.sh
+++ scripts/publish-experimental.sh
@@ -4,4 +4,4 @@ set -euoi pipefail
 
 npm version --allow-same-version --git-tag-version false "0.0.0-experimental-$(git rev-parse --short HEAD)-$(date +%Y%m%d)"
 
-npm publish --tag experimental
+npm publish --tag experimental --access public
